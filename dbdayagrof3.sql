0bafee4d0ebec64b5f11ba488150478ffcba3fc6
LAB11_USER ON WS23 <L11Y14W23@citulab.net>
2024-04-06 14:48:24 +0800

    added sql

--- dbdayagrof3.sql
+++ dbdayagrof3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2024 at 07:10 AM
+-- Generation Time: Apr 06, 2024 at 08:41 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -24,12 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tblproducts`
+--
+
+CREATE TABLE `tblproducts` (
+  `ProductID` int(6) NOT NULL,
+  `ProductName` varchar(30) NOT NULL,
+  `ProductPrice` float NOT NULL,
+  `ProductDesc` varchar(10000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblproducts`
+--
+
+INSERT INTO `tblproducts` (`ProductID`, `ProductName`, `ProductPrice`, `ProductDesc`) VALUES
+(1, 'ArduinoUno', 350, 'The Arduino Uno is a popular microcontroller board based on the ATmega328P. It features 14 digital input/output pins, 6 analog inputs, a 16 MHz quartz crystal, USB connection, power jack, ICSP header, and a reset button. It\'s an excellent choice for beginners and professionals alike for various projects.'),
+(2, 'Arduino Nano', 250, 'The Arduino Nano is a compact yet powerful microcontroller board based on the ATmega328. It\'s similar to the Arduino Uno but in a smaller form factor, making it suitable for projects where space is limited. It features 14 digital input/output pins, 8 analog inputs, USB connectivity, and more.'),
+(4, 'Arduino Mega 2560', 1000, 'The Arduino Mega 2560 is a robust microcontroller board based on the ATmega2560. It\'s designed for projects that require more I/O pins and memory. With 54 digital input/output pins, 16 analog inputs, a larger flash memory size, and more, it\'s suitable for complex projects and prototyping.'),
+(5, 'DHT22 Temperature and Humidity', 200, 'The DHT22 sensor is capable of measuring both temperature and humidity with high accuracy. It uses a digital signal output, making it easy to interface with microcontrollers like Arduino. Ideal for weather stations, environmental monitoring, and HVAC systems.\r\n\r\n'),
+(6, 'MPU-6050 Gyroscope and Acceler', 300, 'The MPU-6050 sensor combines a gyroscope and accelerometer in a single chip. It provides precise motion sensing capabilities, making it suitable for applications such as motion tracking, gesture recognition, and drone stabilization.');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbluseraccount`
 --
 
 CREATE TABLE `tbluseraccount` (
   `acctid` int(3) NOT NULL,
-  `emailadd` varchar(20) NOT NULL,
+  `emailadd` varchar(255) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
   `usertype` varchar(20) NOT NULL
@@ -40,7 +64,7 @@ CREATE TABLE `tbluseraccount` (
 --
 
 INSERT INTO `tbluseraccount` (`acctid`, `emailadd`, `username`, `password`, `usertype`) VALUES
-(4, 'francisdayagro24@gma', 'gypsycrusader ', 'AJPRTCJ3', '');
+(9, 'jmsacbl@gmail.com', 'jmsacbl', '123456', '');
 
 -- --------------------------------------------------------
 
@@ -61,11 +85,17 @@ CREATE TABLE `tbluserprofile` (
 --
 
 INSERT INTO `tbluserprofile` (`userid`, `firstname`, `lastname`, `gender`, `birthday`) VALUES
-(7, 'Francis ', 'Dayagro ', 'Male ', '0000-00-00');
+(15, 'James', 'Acabal', 'Male', '2024-04-08');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tblproducts`
+--
+ALTER TABLE `tblproducts`
+  ADD PRIMARY KEY (`ProductID`);
 
 --
 -- Indexes for table `tbluseraccount`
@@ -84,16 +114,22 @@ ALTER TABLE `tbluserprofile`
 --
 
 --
+-- AUTO_INCREMENT for table `tblproducts`
+--
+ALTER TABLE `tblproducts`
+  MODIFY `ProductID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT for table `tbluseraccount`
 --
 ALTER TABLE `tbluseraccount`
-  MODIFY `acctid` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `acctid` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbluserprofile`
 --
 ALTER TABLE `tbluserprofile`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
